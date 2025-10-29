4e59a4c38576f248e82c9a5f708b4a3806bb7b90
putrinaq <100805@siswa.unimas.my>
2025-10-29 19:37:50 +0800

    firebase

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+// vite.config.js
+import { defineConfig } from "vite";
+
+export default defineConfig({
+	root: "portfolio",
+	build: {
+		outDir: "../dist", // optional: keeps build output outside portfolio/
+	},
+});
